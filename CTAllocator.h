aa2864887d3a5eb6fb5b7153dbde2c40bcd2b8ed
Carlo Tortorella <carlo@johannesburg.local>
2013-10-23 12:06:24 +1100

    Added Javadoc style documentation for CTAllocator

--- CTAllocator.h
+++ CTAllocator.h
@@ -8,15 +8,43 @@
 
 #pragma once
 
+
+/**
+ * An object that acts as a front to malloc, realloc and free in order to keep track of allocated memory.
+ **/
 typedef struct
 {
 	unsigned long count;
 	void ** objects;
 } CTAllocator;
 
+/**
+ * Create a CTAllocator to use in allocation and destruction of memory.
+ * @return Returns an initialised CTAllocator and can be used to allocate and deallocate memory.
+ **/
 CTAllocator * CTAllocatorCreate();
+/**
+ * Deallocate all memory associated with a specified CTAllocator, including that used by the allocator itself.
+ * @param allocator A properly initialised CTAllocator that was created with CTAllocatorCreate.
+ **/
 void CTAllocatorRelease(CTAllocator * restrict allocator);
-
+/**
+ * Allocate memory and add a pointer to it to the objects array within the specified CTAllocator, to be used when CTAllocatorRelease is called.
+ * @return Returns a block of memory created with malloc.
+ **/
 void * CTAllocatorAllocate(CTAllocator * restrict allocator, unsigned long size);
+/**
+ * Reallocate memory to be the specified size if a pointer to it exists in the objects array.
+ * @param allocator A properly initialised CTAllocator that was created with CTAllocatorCreate.
+ * @param ptr A pointer to the chunk of memory to resize.
+ * @param size The size to reallocate the block as.
+ * @return Returns a block of memory created with realloc.
+ **/
 void * CTAllocatorReallocate(CTAllocator * restrict allocator, void * ptr, unsigned long size);
+/**
+ * Deallocate memory at the given address and remove it from the objects array.
+ * @param allocator A properly initialised CTAllocator that was created with CTAllocatorCreate.
+ * @param ptr A pointer to the chunk of memory to deallocate.
+ * @return A dark void, filled with eldritch creatures, the sight of which would cause any human to lose all connections to reality.
+ **/
 void CTAllocatorDeallocate(CTAllocator * restrict allocator, void * ptr);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 CTLibrary
 =========
 
-A C library inspired by CoreFoundation and attempting to bridge the power gap between C and C++
+A POSIX C library inspired by CoreFoundation and attempting to bridge the power gap between C and C++
